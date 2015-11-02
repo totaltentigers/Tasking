af16f68040656edf399dab2fab1356461cd9f001
Jake Moritz <jakemoritz1@gmail.com>
2015-11-02 13:45:09 -0500

    User name, email, image, and cover image now display in side nav

--- app/src/main/java/me/jakemoritz/tasking/MainActivity.java
+++ app/src/main/java/me/jakemoritz/tasking/MainActivity.java
@@ -3,6 +3,17 @@ package me.jakemoritz.tasking;
 import android.content.Intent;
 import android.content.IntentSender;
 import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
+import android.graphics.Rect;
+import android.graphics.RectF;
+import android.graphics.drawable.BitmapDrawable;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -11,13 +22,22 @@ import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.Scopes;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.Scope;
 import com.google.android.gms.plus.Plus;
+import com.google.android.gms.plus.model.people.Person;
+
+import java.io.InputStream;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener, AccountDialogPreference.OnSignOutListener,
@@ -38,6 +58,8 @@ public class MainActivity extends AppCompatActivity
     private boolean mShouldResolve = false;
 
     private boolean signingOut = false;
+    private boolean updatingUserInfo = false;
+    private View header;
 
     public String getmEmail() {
         return mEmail;
@@ -45,6 +67,10 @@ public class MainActivity extends AppCompatActivity
 
     private String mEmail;
 
+    ImageView navUserAvatar;
+    TextView navUserName;
+    TextView navUserEmail;
+    LinearLayout navUserCover;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -69,10 +95,34 @@ public class MainActivity extends AppCompatActivity
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
+        header = LayoutInflater.from(this).inflate(R.layout.nav_header_main, null);
+        navigationView.addHeaderView(header);
+
+        navUserAvatar = (ImageView) header.findViewById(R.id.user_avatar);
+        navUserName = (TextView) header.findViewById(R.id.user_name);
+        navUserEmail = (TextView) header.findViewById(R.id.user_email);
+        navUserCover = (LinearLayout) header.findViewById(R.id.user_cover);
+
+        updateNavUserInfo();
+
         // Initialize default fragment
         getFragmentManager().beginTransaction()
                 .replace(R.id.content_main, new TaskListFragment())
                 .commit();
+    }
+
+    public void updateNavUserInfo(){
+        // Build GoogleApiClient with access to basic profile
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(Plus.API)
+                .addScope(new Scope(Scopes.PROFILE))
+                .addScope(new Scope(Scopes.EMAIL))
+                .build();
+
+        updatingUserInfo = true;
+        mGoogleApiClient.connect();
     }
 
     @Override
@@ -143,6 +193,78 @@ public class MainActivity extends AppCompatActivity
 
             startActivity(new Intent(this, HelperActivity.class));
         }
+        else if (updatingUserInfo){
+            final Person user = Plus.PeopleApi.getCurrentPerson(mGoogleApiClient);
+
+            navUserName.setText(user.getDisplayName());
+
+            SharedPreferences sharedPreferences = getSharedPreferences("PREFS_ACC", 0);
+            String mEmail = sharedPreferences.getString("email", null);
+            navUserEmail.setText(mEmail);
+
+            new AsyncTask<String, Void, Bitmap>(){
+                @Override
+                protected void onPostExecute(Bitmap bitmap) {
+                    Bitmap circle = getCircleBitmap(bitmap);
+                    Bitmap scaled = Bitmap.createScaledBitmap(circle, 168, 168, true);
+                    navUserAvatar.setImageBitmap(scaled);
+                }
+
+                @Override
+                protected Bitmap doInBackground(String... params) {
+                    try {
+                        URL url = new URL(params[0]);
+                        InputStream in = url.openStream();
+                        return BitmapFactory.decodeStream(in);
+                    } catch (Exception e){
+                        Log.e(TAG, e.toString());
+                    }
+                    return null;
+                }
+            }.execute(user.getImage().getUrl());
+
+            new AsyncTask<String, Void, Bitmap>(){
+                @Override
+                protected void onPostExecute(Bitmap bitmap) {
+                    navUserCover.setBackground(new BitmapDrawable(getResources(), bitmap));
+                }
+
+                @Override
+                protected Bitmap doInBackground(String... params) {
+                    try {
+                        URL url = new URL(params[0]);
+                        InputStream in = url.openStream();
+                        return BitmapFactory.decodeStream(in);
+                    } catch (Exception e){
+                        Log.e(TAG, e.toString());
+                    }
+                    return null;
+                }
+            }.execute(user.getCover().getCoverPhoto().getUrl());
+        }
+    }
+
+    private Bitmap getCircleBitmap(Bitmap bitmap){
+        final Bitmap output = Bitmap.createBitmap(bitmap.getWidth(), bitmap.getHeight(), Bitmap.Config.ARGB_8888);
+
+        final Canvas canvas = new Canvas(output);
+
+        final int color = Color.RED;
+        final Paint paint = new Paint();
+        final Rect rect = new Rect(0, 0, bitmap.getWidth(), bitmap.getHeight());
+        final RectF rectF = new RectF(rect);
+
+        paint.setAntiAlias(true);
+        canvas.drawARGB(0, 0, 0, 0);
+        paint.setColor(color);
+        canvas.drawOval(rectF, paint);
+
+        paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
+        canvas.drawBitmap(bitmap, rect, rect, paint);
+
+        bitmap.recycle();
+
+        return output;
     }
 
     @Override
