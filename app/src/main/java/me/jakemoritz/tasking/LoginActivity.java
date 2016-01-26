ea3e19d00499a465b869646e229bd207fd8ae063
Jake Moritz <jakemoritz1@gmail.com>
2016-01-25 21:20:26 -0500

    Fixed navigation bar unhiding on Login screen with screen interaction

--- app/src/main/java/me/jakemoritz/tasking/LoginActivity.java
+++ app/src/main/java/me/jakemoritz/tasking/LoginActivity.java
@@ -66,7 +66,8 @@ public class LoginActivity extends AppCompatActivity implements GoogleApiClient.
                 | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
                 | View.SYSTEM_UI_FLAG_LOW_PROFILE
                 | View.SYSTEM_UI_FLAG_FULLSCREEN
-                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
 
         getWindow().getDecorView().setSystemUiVisibility(mUIFlag);;
     }
